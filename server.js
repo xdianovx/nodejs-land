bd7734b46683ce8e87774c55802f3f475624b792
Dianov Evgenii <justsmm@mail.ru>
2020-07-13 11:47:03 +0300

    Главная страница

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const express = require('express');
+
+const app = express();
+
+const handlebars = require('express-handlebars')
+    .create({ defaultLayout: 'main' });
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        layout: 'main',
+    });
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express starnted on port http://localhost:' + app.get('port'))
+})
